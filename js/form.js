7aa11c10f9a85597ecf59c72a9de124853887604
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-03-23 15:46:39 +0300

    Валидация и форма

--- js/form.js
+++ js/form.js
@@ -1,100 +1,99 @@
 import { isEscKey } from './util.js';
 
-const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i
-const HASHTAG_MAX_COUNT = 5
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const HASHTAG_MAX_COUNT = 5;
 
 
 const body = document.querySelector('body');
-const imgUpload = document.querySelector('.img-upload')
-const formUpload = document.querySelector('.img-upload__form')
-const imgUploadOverlay = document.querySelector('.img-upload__overlay')
-const buttonUpload = formUpload.querySelector('.img-upload__label')
-const buttonImgUploadCansel = formUpload.querySelector('.img-upload__cancel')
-const buttonImgUploadSubmit = formUpload.querySelector('#upload-submit')
-const imjUploadPreveiw = formUpload.querySelector('.img-upload__preview')
-const scaleControlValue = formUpload.querySelector('.scale__control--value')
-const commentTextDescription = formUpload.querySelector('.text__description')
-const hashtagTextField = formUpload.querySelector('.text__hashtags')
+const imgUpload = document.querySelector('.img-upload');
+const formUpload = document.querySelector('.img-upload__form');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const buttonUpload = formUpload.querySelector('.img-upload__label');
+const buttonImgUploadCansel = formUpload.querySelector('.img-upload__cancel');
+// const buttonImgUploadSubmit = formUpload.querySelector('#upload-submit');
+const imjUploadPreveiw = formUpload.querySelector('.img-upload__preview');
+const scaleControlValue = formUpload.querySelector('.scale__control--value');
+const commentTextDescription = formUpload.querySelector('.text__description');
+const hashtagTextField = formUpload.querySelector('.text__hashtags');
 
 const pristine = new Pristine(formUpload, {
-    classTo: 'img-upload__wrapper', 
-    errorTextParent: 'img-upload__field-wrapper'
-})
+  classTo: 'img-upload__wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
 
 function onOpenForm () {
-    imgUploadOverlay.classList.remove('hidden')
-    body.classList.add('modal-open');
-    document.addEventListener('keydown', onDocumentEscKeyDown)
-    onBlockEsc()
-    onFormSubmit()
-    
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentEscKeyDown);
+  onBlockEsc();
+  onFormSubmit();
+
 }
 
 function onCloseForm () {
-    formUpload.reset()
-    imgUploadOverlay.classList.add('hidden')
-    body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onDocumentEscKeyDown)
-    scaleControlValue.value = `${100}%`
-    formUpload.removeAttribute('style')
+  formUpload.reset();
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentEscKeyDown);
+  scaleControlValue.value = `${100}%`;
+  formUpload.removeAttribute('style');
 }
 
 
 function onDocumentEscKeyDown (evt) {
-    if (isEscKey(evt)) {
-      evt.preventDefault();
-      onCloseForm();
-    }
+  if (isEscKey(evt)) {
+    evt.preventDefault();
+    onCloseForm();
   }
+}
 
 function onBlockEsc () {
-    commentTextDescription.addEventListener('keydown', (evt) => {
-        if(evt.key === 'Escape') {
-            evt.stopPropagation()
-        }
-    })
+  commentTextDescription.addEventListener('keydown', (evt) => {
+    if(evt.key === 'Escape') {
+      evt.stopPropagation();
+    }
+  });
 }
 
 const hasValidCount = (hashtags) =>
   hashtags.length <= HASHTAG_MAX_COUNT;
 
-const isValidTag = (tag) => VALID_SYMBOLS.test(tag)
+const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
 
 const hasUniqeTags = (tags) => {
-    const lowerCaseTags = tags.map((tag) => tag.toLowerCase())
-    return lowerCaseTags.length === new Set(lowerCaseTags).size
-}
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
 
 const validateTags = (value) => {
-    const tags = value
-        .split(' ')
-        .filter((tag) => tag.trim().length)
-    return hasValidCount(tags) && hasUniqeTags(tags) && tags.every(isValidTag)
-}
+  const tags = value
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqeTags(tags) && tags.every(isValidTag);
+};
 
 
 pristine.addValidator(
-    hashtagTextField, 
-    validateTags,
-    'Неправильно заполнены хэштеги',
-)
+  hashtagTextField,
+  validateTags,
+  'Неправильно заполнены хэштеги',
+);
 
 const onFormSubmit = function () {
-    formUpload.addEventListener('submit', (evt) => {
-        evt.preventDefault()
-        const isValid = pristine.validate()
-        if (isValid) {
-            console.log('Send')
-        } else {
-            console.log('Dont Send')
-        }
-    })
-    
-}
+  formUpload.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    if (isValid) {
+      console.log('Send');
+    } else {
+      console.log('Dont Send');
+    }
+  });
 
-buttonUpload.addEventListener('click', onOpenForm)
-buttonImgUploadCansel.addEventListener('click', onCloseForm)
+};
 
+buttonUpload.addEventListener('click', onOpenForm);
+buttonImgUploadCansel.addEventListener('click', onCloseForm);
+onFormSubmit();
 
-
-export {imgUpload, imjUploadPreveiw}
+export {imgUpload, imjUploadPreveiw};
