e21017d3f8ffac2587ea46518aec4ad1c3b1ec77
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-03-24 18:48:25 +0300

    Комментарии финал

--- js/bigFoto.js
+++ js/bigFoto.js
@@ -18,63 +18,43 @@ const COMMENTS_PER_PORTION = 5
 
 let countShow = 0
 
+let allComments = []
+
+const createComments = function  (dataComments) {
+  socialComments.innerHTML = '';
+  const commentsListFragment = document.createDocumentFragment();
+  dataComments.map((comment) => {
+    commentsListFragment.appendChild(createComment(comment));
+  });
+  
+  return socialComments.appendChild(commentsListFragment);
+}
+
 const renderBigFoto = function () {
   allPictures.addEventListener('click', (evt) => {
     evt.preventDefault();
     if (evt.target.closest('a')) {
       onClickPicture();
       const i = evt.target.closest('a').dataset.index;
-      const allComments = pictures[i].comments
+      allComments = pictures[i].comments
 
       bigPictureImg.src = pictures[i].url;
       socialCaption.textContent = pictures[i].description;
       bigPictureLike.textContent = pictures[i].likes;
       bigPictureComment.textContent = pictures[i].comments.length;
 
-      
-
-      const createComments = function  (dataComments) {
-        socialComments.innerHTML = '';
-        const commentsListFragment = document.createDocumentFragment();
-        dataComments.map((comment) => {
-          commentsListFragment.appendChild(createComment(comment));
-        });
-        
-        return socialComments.appendChild(commentsListFragment);
-      }
-      
-
-      countShow += COMMENTS_PER_PORTION
-      
+      countShow = 0
+      addComments()
       if (allComments.length <= countShow) {
         socialCommentMin.textContent = allComments.length
         socialCommentsButton.classList.add('hidden')
-        createComments(allComments)
       } else {
-        let commentSlice = allComments.slice(0, countShow)
-        createComments(commentSlice)
-        socialCommentsButton.addEventListener('click', () => {
-          countShow += COMMENTS_PER_PORTION
-          commentSlice = allComments.slice(0, countShow)
-          createComments(commentSlice)
-          socialCommentMin.textContent = countShow
-          if(allComments.length <= countShow) {
-            socialCommentMin.textContent = allComments.length
-            socialCommentsButton.classList.add('hidden')
-          }
-        })
-    
-
+        socialCommentMin.textContent = countShow
       }
+      socialCommentsButton.addEventListener('click', addComments) 
     }
   });
 };
-
-function onLoadComments () {
-  for (let i = 0; i < countShow; i++) {
-
-  }
-}
 
 
 closeButtonBigPicture.addEventListener('click', () => {
@@ -85,8 +65,18 @@ function onClickPicture () {
   bigPicture.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentEscKeyDown);
   body.classList.add('modal-open');
+  socialCommentsButton.addEventListener('click', addComments)
 }
 
+function addComments () {
+  countShow += COMMENTS_PER_PORTION
+  let commentSlice = allComments.slice(0, countShow)
+  createComments(commentSlice)
+  socialCommentMin.textContent = commentSlice.length
+  if (allComments.length <= countShow) {
+    socialCommentsButton.classList.add('hidden')
+  }
+}
 
 function onClosePicture () {
   bigPicture.classList.add('hidden');
@@ -94,6 +84,7 @@ function onClosePicture () {
   body.classList.remove('modal-open');
   countShow = 0
   socialCommentsButton.classList.remove('hidden')
+  socialCommentsButton.removeEventListener('click', addComments)
 }
 
 function onDocumentEscKeyDown (evt) {
