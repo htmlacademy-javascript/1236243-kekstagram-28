ad163f0e8d349caee22afe029452ea23946053dc
Keks <keks@htmlacademy.ru>
2023-03-30 11:11:22 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,11 @@
+import { renderMiniFoto } from'./miniatures.js';
+import { renderBigFoto } from './bigFoto.js';
+import './form.js';
+import { changeScaleMin, changeScaleMax } from './scale.js';
+
+
+renderMiniFoto();
+renderBigFoto();
+
+changeScaleMin();
+changeScaleMax();
