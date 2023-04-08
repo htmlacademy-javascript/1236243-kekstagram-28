5caf98aef36c5f0e727bb2ce6a6a14141a27dc48
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-04-08 14:17:51 +0300

    Фото на память v1.1

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ import { renderBigFoto } from './bigFoto.js';
 import {setUserSubmit} from './form.js';
 import { getData } from './fetch.js';
 import { filterPictures } from './filters.js';
-import { debounce } from './util.js';
+import { debounce, showAlert} from './util.js';
+
 
 const RENDER_DELAY = 500;
 
-getData((data) => {
-  renderMiniFoto(data);
-  filterPictures(debounce(renderMiniFoto, RENDER_DELAY), data);
-  renderBigFoto(data);
-});
-
-
+try {
+    getData((data) => {
+        renderMiniFoto(data);
+        filterPictures(debounce(renderMiniFoto, RENDER_DELAY), data);
+        renderBigFoto(data);
+        });
+} catch (err) {
+    showAlert(err.message)
+}
 setUserSubmit();
+
