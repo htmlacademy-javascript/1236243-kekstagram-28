d5d5ea87b72c90814bca267ddc9e1831b83b25b0
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-04-08 13:44:49 +0300

    Фото на память

---
+++ js/foto.js
@@ -0,0 +1,17 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('.img-upload__wrapper input[type=file]');
+const preview = document.querySelector('.user-pic');
+
+const changeURL = () => {
+  const foto = fileChooser.files[0];
+  const fotoName = foto.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fotoName.endsWith(it));
+
+  if (matches) {
+    preview.src = URL.createObjectURL(foto);
+  }
+};
+
+export {changeURL};
