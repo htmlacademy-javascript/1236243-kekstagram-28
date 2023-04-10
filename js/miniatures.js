d80c29950a1daa2a141ac71c4802f448935e6858
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-04-10 22:18:26 +0300

    Финал v1.2

--- js/miniatures.js
+++ js/miniatures.js
@@ -5,19 +5,19 @@ const pictureTemplate = document.querySelector('#picture') // находим ш�
   .querySelector('.picture');
 
 const renderMiniFoto = function (dataPictures) {
+  allPictures.querySelectorAll('.picture').forEach((element) => element.remove())
   const picturesFragment = document.createDocumentFragment();
-  allPictures.querySelectorAll('.picture').forEach((element) => element.remove());
   dataPictures
-    .forEach((picture, index) => {
+    .forEach((picture) => {
       const pictureElement = pictureTemplate.cloneNode(true);
       pictureElement.querySelector('.picture__img').src = picture.url;
       pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
       pictureElement.querySelector('.picture__likes').textContent = picture.likes;
-      pictureElement.dataset.index = index;
+      pictureElement.dataset.index = picture.id;
       picturesFragment.appendChild(pictureElement);
 
     });
-
+  
   allPictures.appendChild(picturesFragment);
 
 };
